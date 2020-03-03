16938734b942b1d2f598292610c259a0be0fc262
Mia Dugas <miadugas@outlook.com>
2020-03-02 18:09:15 -0700

    react-chat

---
+++ client/build/precache-manifest.99e92597edd218ca47f292f79f92e820.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6896c5fc7be2fd9e8599b8af1129726c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "193cc64779507b1a9213",
+    "url": "/static/css/main.ef0f8c5c.chunk.css"
+  },
+  {
+    "revision": "a3976446e0fcfca0a2b1",
+    "url": "/static/js/2.2a248f86.chunk.js"
+  },
+  {
+    "revision": "e1345d47b220a41451b8c23eb83bc290",
+    "url": "/static/js/2.2a248f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193cc64779507b1a9213",
+    "url": "/static/js/main.0d1ab5d2.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "a05e7e1bf06af6a2922bbed66e62f42e",
+    "url": "/static/media/IMG_6431.a05e7e1b.JPEG"
+  }
+]);
